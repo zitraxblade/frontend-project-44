5492f1a06c8a1bf672e863fe1949c7e12e71250c
Егор Бажайкин <egor50311@gmail.com git config --global user.name zitraxbladegit commit -m 'hexlet'git branch -M maingit remote add origin git@github.com:zitraxblade/hexlet-js.gitgit push -u origin maingit remote add origin git@github.com:zitraxblade/hexlet-js.gitgit branch -M maingit push -u origin maingit statusE1sqirexxtentionmamaruss.50311LSPWD$ git config --global user.email egor50311@gmail.com>
2024-07-25 18:26:22 +0300

    3 etap

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node.
-console.log('Welcome to the Brain Games!')
-import readlineSync from 'readline-sync'
-var userName = readlineSync . question ('May I have your name?') 
-console.log ( 'Hello'  +  userName  +  '!');
+import startGreeting from '../src/cli.js';
+
+startGreeting();
