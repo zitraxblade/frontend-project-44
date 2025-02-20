1e3376db833d98fb09a8ec16b2898712c798bb8f
zitraxblade <egor50311@gmail.com>
2025-02-20 09:16:41 +0300

    3etap

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync'
-
- console.log('Welcome to the Brain Games!');
-  const user = () =>
-     {  const name = readlineSync.question('May I haver your name?');
+console.log('brain-games');
+  const user = () => {
+console.log('Welcome to the Brain Games!');
+      const name = readlineSync.question('May I haver your name?');
     return `Hello, ${name}!`
 }
 export default user;
