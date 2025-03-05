35f617b9ceda5ae2624ff1852252b6a1deba3f62
zitraxblade <egor50311@gmail.com>
2025-03-05 12:08:31 +0300

    proverka

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,40 +1,38 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
 const evenNumberGame = () => {
-    const greetUser = () => {
-        console.log('Welcome to the Brain Games!');
-        const name = readlineSync.question('May I have your name? ');
-        return console.log(`Hello, ${name}!`);
-    };
-    const names = greetUser();
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
- const playerWin = () =>{
- const winer = (finish) => {
-    finish = 3;
-    let sum = 0;
-    const num = getRandom(100);
-    const delenie = num % 2;
-    while (sum === finish) {
-        if (delenie === 0) {
-            if ( userAnswer === 'yes') {
-            console.log('Correct!')
-            sum = sum + 1;
-        } else {
-            console.log(`' ${userAnswer} ' is wrong answer ;(. Correct answer was ' no '.`);
-                return false;
-        }
+const user = () =>{
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+return console.log(`Hello, ${name}!`);
     }
- }
-
-
- }
-
-
-
-
+const name = user()
+console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+const playerWin =(max) => {
+const isEven = (num) => {
+        const abs = num % 2 === 0
+        return abs;
+      }
+const randoms = getRandom(100);
+console.log(`Question: ${randoms}`);
+const userAnswer = readlineSync.question('Your answer: ');
+let sum = 0;
+while (sum <= max) {
+    if (isEven(randoms) && userAnswer === 'yes' || isEven(randoms) && userAnswer === 'no') {
+        sum = sum + 1;
+       console.log('Correct!')
+    } else {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
+        console.log(`Let's try again, ${name}`);
+        return false;
+    }
+}
+    };
+    playerWin(3);
 };
+
 const getRandom = (max) => {
     return Math.floor(Math.random() * max);
 };
  evenNumberGame();
+ 
