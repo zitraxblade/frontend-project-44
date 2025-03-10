810fdf44cda9725fb055855db50940c55646f342
zitraxblade <egor50311@gmail.com>
2025-03-10 18:52:28 +0300

    5 etap

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,38 +1,40 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-const evenNumberGame = () => {
-const user = () =>{
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-return console.log(`Hello, ${name}!`);
-    }
-const name = user()
-console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-const playerWin =(max) => {
-const isEven = (num) => {
-        const abs = num % 2 === 0
-        return abs;
-      }
-const randoms = getRandom(100);
-console.log(`Question: ${randoms}`);
-const userAnswer = readlineSync.question('Your answer: ');
-let sum = 0;
-while (sum <= max) {
-    if (isEven(randoms) && userAnswer === 'yes' || isEven(randoms) && userAnswer === 'no') {
-        sum = sum + 1;
-       console.log('Correct!')
+// Функция привествования.
+function helloUsser () {
+    console.log('Welcome to the Brain Games!');
+    const names = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${names}!`)
+    return names;
+}
+const names = helloUsser();
+// Функция рандомное число.
+function randomNum () {
+    return Math.floor(Math.random() * 100);
+}
+// Функция проверка чётности числа.
+function isEven(number) {
+    const two = number % 2 === 0;
+    return two;
+}
+// Функция движка игры.
+function gameStart () {
+    console.log("Answer 'yes' if number even otherwise answer 'no'");
+    for (let i = 0; i < 3; i++) {
+    const random = randomNum()
+    console.log(`Question: ${random}`);
+    const userAnswer = readlineSync.question('Your Answer:')
+    const correctAnswer = isEven(random) ? 'yes' : 'no';
+    if (correctAnswer === userAnswer.trim()) {
+        console.log('Correct!');
     } else {
-        console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
-        console.log(`Let's try again, ${name}`);
-        return false;
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was "no".\n Let's try again ${names}`);
+    return false;
+    }
+    if(isCorrectGame) {
+    console.log(`Congratulations, ${names}`)
+    }
     }
 }
-    };
-    playerWin(3);
-};
-
-const getRandom = (max) => {
-    return Math.floor(Math.random() * max);
-};
- evenNumberGame();
- 
+gameStart();
+export default gameStart;
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync'
 
-  const user = () => {
-    console.log('Brain-Games')
+const user = () => {
 console.log('Welcome to the Brain Games!');
-      const name = readlineSync.question('May I haver your name?');
-    return console.log(`Hello, ${name}!`); 
+const name = readlineSync.question('May I haver your name?');
+console.log(`Hello, ${name}!`); 
 }
 
 export default user;
