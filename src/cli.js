40027886ec71eb7e65d07a49c801ac41d4e9d041
zitraxblade <egor50311@gmail.com>
2025-04-11 20:01:19 +0300

    final

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync'
 
 const user = () => {
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I haver your name?');
+const name = readlineSync.question('May I have your name?');
 console.log(`Hello, ${name}!`); 
 }
 
