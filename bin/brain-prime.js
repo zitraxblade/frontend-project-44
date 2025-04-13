16ef92a50cf6b3d451eb4d4a4367687de9457564
zitraxblade <egor50311@gmail.com>
2025-04-13 15:43:07 +0300

    fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
-// This is the brain-progression game script
 import runPrime from "../src/games/brainPrime.js";
 runPrime();
--- spec/50_brain_progression_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe 'bin/brain-progression' do
-  it 'has description' do
-    file_path = './bin/brain-progression'
-    expect(File).to exist(file_path)
-    file_content = File.read(file_path)
-    expect(file_content).to start_with('#!/usr/bin/env node')
-  end
