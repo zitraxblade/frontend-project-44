c03c097b072010dbc771c17b2717d42915d508ca
zitraxblade <egor50311@gmail.com>
2025-04-14 14:57:06 +0300

    fix

--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -5,14 +5,14 @@ const operations = ['+', '*', '-'];
 
 const calculator = (num1, num2, operator) => {
   switch (operator) {
-  case '+':
-  return num1 + num2;
-  case '-':
-  return num1 - num2;
-  case '*':
-  return num1 * num2;
-  default:
-  return null;
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
   }
 };
 
